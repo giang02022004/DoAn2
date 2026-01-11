9c3a584656c8dbec85ceec7f822f9e76a9237de1
giang220239-cmd <giang220239cmd@gmail.com>
2026-01-11 20:50:42 +0700

    Thêm test cases

---
+++ src/test/java/org/example/doan2/DoAn2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.doan2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAn2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
