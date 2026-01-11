b2f71fabf32e58210b2627fcc0ed9130843f06b6
giang220239-cmd <giang220239cmd@gmail.com>
2026-01-11 22:33:37 +0700

    Cập nhật Controller hiển thị danh sách sản phẩm

--- src/main/java/org/example/doan2/controller/IndexController.java
+++ src/main/java/org/example/doan2/controller/IndexController.java
@@ -1,13 +1,20 @@
 package org.example.doan2.controller;
-
+import org.example.doan2.service.SanPhamService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 class IndexController {
+    private final SanPhamService sanPhamService;
+    public IndexController(SanPhamService sanPhamService) {
+        this.sanPhamService = sanPhamService;
+    }
+
     @GetMapping("/index")
-    public String home() {
-        return "index"; // index.html trong templates
+    public String index(Model model) {
+        model.addAttribute("dsSanPham", this.sanPhamService.getTop15MoiNhat());
+        return "index";
     }
 }
