b19af07db9a9f6cdf24de8c0485b3a7ba35dd477
giang220239-cmd <giang220239cmd@gmail.com>
2026-01-11 22:33:37 +0700

    Thêm Repository cho SanPham

---
+++ src/main/java/org/example/doan2/repository/SanPhamRepository.java
@@ -0,0 +1,7 @@
+package org.example.doan2.repository;
+
+import org.example.doan2.entity.SanPham;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SanPhamRepository extends JpaRepository<SanPham, Integer> {
+}
