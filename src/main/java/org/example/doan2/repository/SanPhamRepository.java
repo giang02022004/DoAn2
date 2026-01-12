7a1437f90870fd153a1c8db338564c05000e0fbc
giang220239-cmd <giang220239cmd@gmail.com>
2026-01-12 10:53:12 +0700

    Cập nhật SanPhamRepository - thêm method tìm kiếm theo loại sản phẩm

--- src/main/java/org/example/doan2/repository/SanPhamRepository.java
+++ src/main/java/org/example/doan2/repository/SanPhamRepository.java
@@ -1,7 +1,15 @@
 package org.example.doan2.repository;
 
 import org.example.doan2.entity.SanPham;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SanPhamRepository extends JpaRepository<SanPham, Integer> {
+    Page<SanPham> findByLoaiSanPham_TenLoaiOrderByIdDesc(
+            String tenLoai,
+            Pageable pageable
+    );
 }
